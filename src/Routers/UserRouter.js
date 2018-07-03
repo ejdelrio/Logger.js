dcfbe5f2d45c055897da056649c9705da1e996a5
Eddie <Eddie@Edwins-MBP.PK5001Z>
2018-07-02 17:51:39 -0700

    Wrote function for Basic Auhorization

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -8,7 +8,48 @@ const UserRouter = new Router();
 
 function basicAuth(req, res, next)
 {
+  if(!req)
+  {
+    let responseError = createError(400, "Request is Null.");
+    next(responseError);
+  }
 
+  let { authorization } = req.headers;
+
+  if(!authorization)
+  {
+    let responseError = createError(400, "Authorization Header Required.");
+    return next(responseError);
+  }
+
+  let base64Header = authHeader.split('Basic ')[1];
+
+  if(!base64Header)
+  {
+    let responseError = createError(400, "Invalid Authorization Header.");
+    return next(responseError);
+  }
+
+  var utf = new Buffer(base64, 'base64').toString();
+  var credentials = utf.split(':');
+
+  if(!credentials[0])
+  {
+    let responseError = createError(400, "User Name Required.");
+    return next(responseError);
+  }
+
+  if(credentials[1])
+  {
+    let responseError = createError(400, "Password Required.");
+    return next(responseError);
+  }
+  req.auth = {
+    userName: credentials[0],
+    passWord: credentials[1]
+  };
+
+  next();
 }
 
 function signup(req, res, next)
